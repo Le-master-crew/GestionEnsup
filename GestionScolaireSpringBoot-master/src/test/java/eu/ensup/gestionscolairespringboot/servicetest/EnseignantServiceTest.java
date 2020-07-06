2798e3fd13f46db22b4233720d1342d105a92eec
Cyril Gautray <gautray.cyril@hotmail.com>
2020-07-06 17:50:23 +0200

    test

---
+++ GestionScolaireSpringBoot-master/src/test/java/eu/ensup/gestionscolairespringboot/servicetest/EnseignantServiceTest.java
@@ -0,0 +1,15 @@
+package eu.ensup.gestionscolairespringboot.servicetest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EnseignantServiceTest {
+
+	
+	@Test
+	void noterEtudiantTest() {
+		fail("Not yet implemented");
+	}
+
+}
