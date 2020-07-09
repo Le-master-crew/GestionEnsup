8b734b348329578b865588783083458c1de4d862
BenKapoor <benjaminkapoor@yahoo.fr>
2020-07-09 10:06:55 +0200

    ajout cours dans data.sql

--- GestionScolaireSpringBoot-master/src/main/resources/data.sql
+++ GestionScolaireSpringBoot-master/src/main/resources/data.sql
@@ -12,3 +12,12 @@ INSERT INTO `users_roles` (`user_id`, `role_id`) VALUES (4, 2);
 INSERT INTO `users_roles` (`user_id`, `role_id`) VALUES (3, 1);
 INSERT INTO `users_roles` (`user_id`, `role_id`) VALUES (1, 1);
 INSERT INTO `users_roles` (`user_id`, `role_id`) VALUES (2, 2);
+
+
+INSERT INTO `cours` (`id_cours`, `libelle`, `nb_heure`) VALUES (1, 'Cours de JAVA', 1);
+INSERT INTO `cours` (`id_cours`, `libelle`, `nb_heure`) VALUES (2, 'Cours de SPRING', 2);
+INSERT INTO `cours` (`id_cours`, `libelle`, `nb_heure`) VALUES (3, 'Cours de ANGULAR', 1);
+
+INSERT INTO `personne` (`id`, `adresse`, `mail`, `nom`, `prenom`, `telephone`) VALUES (1, "241 rue DUGUESCLIN,
+69003 Lyon","contact@objis.com", "objis", "objis", 012030405);
+INSERT INTO `etudiant` (`date_naissance`, `id`) VALUES ("01/01/01", 1);
